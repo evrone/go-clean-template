c7a7ef55552a5ab057d32d625a89bc86b9c9f200
Misha Nepryakhin <neprja@gmail.com>
2021-04-02 17:00:57 +0400

    [GOT-26] Edited the readme file and removed the Rollbar logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,13 +1,19 @@
 package logger
 
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+)
+
 var appLogger Logger //nolint:gochecknoglobals // it's necessary
 
-func NewAppLogger(zap *ZapLogger, rollbar *RollbarLogger, appName, appVersion string) {
+func NewAppLogger(zapLogger *ZapLogger, appName, appVersion string) {
 	fields := []Field{
 		{"app-name", appName},
 		{"app-version", appVersion},
 	}
-	appLogger = &loggers{zap, rollbar, fields}
+	appLogger = &logger{zapLogger, fields}
 }
 
 func Debug(msg string, fields ...Field) {
@@ -28,4 +34,47 @@ func Error(err error, msg string, fields ...Field) {
 
 func Fatal(err error, msg string, fields ...Field) {
 	appLogger.fatal(err, msg, fields...)
+}
+
+type logger struct {
+	zap           *ZapLogger
+	defaultFields []Field
+}
+
+func (l *logger) debug(msg string, fields ...Field) {
+	fields = append(l.defaultFields, fields...)
+	l.zap.Debug(msg, zapFields(fields)...)
+}
+
+func (l *logger) info(msg string, fields ...Field) {
+	fields = append(l.defaultFields, fields...)
+	l.zap.Info(msg, zapFields(fields)...)
+}
+
+func (l *logger) warn(msg string, fields ...Field) {
+	fields = append(l.defaultFields, fields...)
+	l.zap.Warn(msg, zapFields(fields)...)
+}
+
+func (l *logger) error(err error, msg string, fields ...Field) {
+	err = fmt.Errorf("%s: %w", msg, err)
+
+	fields = append(l.defaultFields, fields...)
+	l.zap.Error(err.Error(), zapFields(fields)...)
+}
+
+func (l *logger) fatal(err error, msg string, fields ...Field) {
+	err = fmt.Errorf("%s: %w", msg, err)
+
+	fields = append(l.defaultFields, fields...)
+	l.zap.Fatal(err.Error(), zapFields(fields)...) // os.Exit()
+}
+
+func zapFields(fields []Field) []zap.Field {
+	s := make([]zap.Field, 0, len(fields))
+	for _, field := range fields {
+		s = append(s, zap.Reflect(field.Key, field.Val))
+	}
+
+	return s
 }
