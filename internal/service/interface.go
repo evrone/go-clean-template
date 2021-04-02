fdf96cefa693f60c3d0f67d4ed0b3e12db4f3380
Misha Nepryakhin <neprja@gmail.com>
2021-04-02 13:35:09 +0400

    [GOT-26] readme

--- internal/service/interface.go
+++
@@ -1,9 +0,0 @@
-// Package service implements application business logic. Each logic group in own file.
-package service
-
-import "github.com/evrone/go-service-template/internal/domain"
-
-type Translation interface {
-	Translate(entity domain.Translation) (domain.Translation, error)
-	History() ([]domain.Translation, error)
-}
