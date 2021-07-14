00c6fb8c1db58abab2fdec3bf5a629821d3a4cfd
Mikhail Grachev <work@mgrachev.com>
2021-07-14 12:28:18 +0300

    Move linters to GH Actions
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- .circleci/bin/reviewdog_dotenv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-# Exit code always 0
-dotenv-linter \
-| reviewdog -f=dotenv-linter -diff="git diff ${DEFAULT_BRANCH}..HEAD" -reporter=github-pr-review
--- .circleci/bin/reviewdog_hadolint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-# Exit code always 0
-git ls-files --exclude='Dockerfile*' --ignored | xargs hadolint \
-| reviewdog -efm="%f:%l %m" -diff="git diff ${DEFAULT_BRANCH}..HEAD" -reporter=github-pr-review
